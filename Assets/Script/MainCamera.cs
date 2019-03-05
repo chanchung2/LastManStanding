331651cec54dc170b888d2964f3844e1aaa33643
ChanChung2 <qkrgksquf00@naver.com>
2019-03-05 19:57:05 +0900

    CameraControll, PlayerControll

---
+++ Assets/Script/MainCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    private float dist = 1.5f;
+
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
+
+    [SerializeField] private float yMinLimit;
+    [SerializeField] private float yMaxLimit;
+
+    [SerializeField] private Transform player;
+
+    private float x; // 카메라의 초기값.
+    private float y;
+
+    Quaternion rotation;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = transform.eulerAngles.y;
+        y = transform.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
+
+        y = Mathf.Clamp(y, yMinLimit, yMaxLimit);
+
+        rotation = Quaternion.Euler(y, x, 0);
+        position = rotation * new Vector3(0, 0, -dist) + player.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+
+    private float h;
+    private float v;
+
+    private Vector3 horizontal;
+    private Vector3 vertical;
+    private Vector3 movement;
+    private Quaternion quaternion;
+
+    [SerializeField] private float range; // 공격 사거리
+    [SerializeField] private LayerMask layerMask;
+
+    private bool isMove = false;
+    private bool isLife = true;
+    private bool isAttack = false;
+
+    private Rigidbody rigidbody;
+    private BoxCollider boxCollider;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform camera;
+
+    private RaycastHit hitInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLife)
+        {
+            Move();
+            TryAttack();
+        }
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            isMove = true;
+            quaternion.eulerAngles = new Vector3(0, camera.rotation.eulerAngles.y, 0);
+        }
+        else
+            isMove = false;
+
+        if (v > 0)
+        {
+            vertical = camera.transform.forward;
+        }
+        else if (v < 0)
+        {
+            vertical = -camera.transform.forward;
+            quaternion.eulerAngles += new Vector3(0, 180, 0);
+        }
+        else
+        {
+            vertical = Vector3.zero;
+        }
+
+        if (h > 0)
+        {
+            horizontal = camera.transform.right;
+            if (v > 0)
+            {
+                quaternion.eulerAngles += new Vector3(0, 45, 0);
+            }
+            else if (v < 0)
+            {
+                quaternion.eulerAngles += new Vector3(0, 315, 0);
+            }
+            else
+            {
+                quaternion.eulerAngles += new Vector3(0, 90, 0);
+            }
+        }
+        else if (h < 0)
+        {
+            horizontal = -camera.transform.right;
+            if (v > 0)
+            {
+                quaternion.eulerAngles += new Vector3(0, 315, 0);
+            }
+            else if (v < 0)
+            {
+                quaternion.eulerAngles += new Vector3(0, 45, 0);
+            }
+            else
+            {
+                quaternion.eulerAngles += new Vector3(0, 270, 0);
+            }
+        }
+        else
+        {
+            horizontal = Vector3.zero;
+        }
+
+        animator.SetBool("Move", isMove);
+        movement = (horizontal + vertical) * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + movement);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, rotationSpeed * Time.deltaTime);
+    }
+
+    private void TryAttack()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("attack"))
+        {
+            isAttack = false;
+        }
+
+        if (!isAttack)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Attack();
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        isAttack = true;
+        animator.SetTrigger("Attack");
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask))
+        {
+            Debug.Log(hitInfo.transform.name);
+            hitInfo.transform.GetComponent<PlayerController>().Die();
+        }
+    }
+
+    private void Die()
+    {
+        isLife = false;
+        animator.SetBool("Die", true);
+    }
+}
+
